5a0377d2e2e0aa67a5e860d9457640a05268815b
2251105 <ckdwn9257@naver.com>
2025-06-10 00:29:25 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -20,6 +20,5 @@ subject = st.text_input("시의 주제를 입력해주세요")
 st.write("주제:" + subject)
 
 if st.button("작성"):
-  with st.spinner("창작중..!")
-     result = chat_model.invoke(subject + "에 대한 시를 써줘")
-     st.write(result.content)
+  result = chat_model.invoke(subject + "에 대한 시를 써줘")
+  st.write(result.content)
